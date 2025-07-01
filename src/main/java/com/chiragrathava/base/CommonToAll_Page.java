fc00149140f08173dc53e00668e49ab0881911d8
ChiragRathava29 <rathavachirag29@gmail.com>
2025-07-01 18:19:25 +0530

    base

--- src/main/java/com/chiragrathava/base/CommonToAll_Page.java
+++ src/main/java/com/chiragrathava/base/CommonToAll_Page.java
@@ -16,10 +16,12 @@ public class CommonToAll_Page {
     public void openOrangeHRM_Url()
     { getDriver().get(Properties_Reader.readKey("ohr_url")); }
 
+
     public void clickElement(WebElement by)
     { by.click(); }
     public void clickElement(By locator)
     { getDriver().findElement(locator).click(); }
+
 
     public void enterInput(By by, String key)
     { getDriver().findElement(by).sendKeys(key); }
